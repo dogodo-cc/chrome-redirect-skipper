12111954eb0c366258b16bbacea7be0ec10acd0b
袁炜海 <yuanweihai25@163.com>
2026-03-28 17:57:25 +0800

    firefox 需要数据声明

--- scripts/build.js
+++ scripts/build.js
@@ -9,8 +9,8 @@ const isChrome = args.includes('--chrome');
 const isFirefox = args.includes('--firefox');
 
 if (!isChrome && !isFirefox) {
-  console.error('Please specify --chrome or --firefox');
-  process.exit(1);
+    console.error('Please specify --chrome or --firefox');
+    process.exit(1);
 }
 
 const distDir = isChrome ? 'dist-chrome' : 'dist-firefox';
@@ -18,52 +18,57 @@ const distPath = join(root, distDir);
 const zipName = isChrome ? 'redirect-skipper-chrome.zip' : 'redirect-skipper-firefox.zip';
 
 (async function () {
-  await rm(distPath, { recursive: true, force: true });
+    await rm(distPath, { recursive: true, force: true });
 
-  await cp(join(root, 'node_modules/'), join(distPath, 'node_modules/'), {
-    recursive: true,
-  });
+    await cp(join(root, 'node_modules/'), join(distPath, 'node_modules/'), {
+        recursive: true,
+    });
 
-  await cp(join(root, '_locales/'), join(distPath, '_locales/'), {
-    recursive: true,
-  });
+    await cp(join(root, '_locales/'), join(distPath, '_locales/'), {
+        recursive: true,
+    });
 
-  await cp(join(root, 'images/'), join(distPath, 'images/'), {
-    recursive: true,
-  });
+    await cp(join(root, 'images/'), join(distPath, 'images/'), {
+        recursive: true,
+    });
 
-  await cp(join(root, 'src/'), join(distPath, 'src/'), {
-    recursive: true,
-  });
+    await cp(join(root, 'src/'), join(distPath, 'src/'), {
+        recursive: true,
+    });
 
-  await copyFile(join(root, 'page-popup.html'), join(distPath, 'page-popup.html'));
+    await copyFile(join(root, 'page-popup.html'), join(distPath, 'page-popup.html'));
 
-  await copyFile(join(root, 'page-options.html'), join(distPath, 'page-options.html'));
+    await copyFile(join(root, 'page-options.html'), join(distPath, 'page-options.html'));
 
-  const manifest = JSON.parse(await readFile(join(root, 'manifest.json')));
+    const manifest = JSON.parse(await readFile(join(root, 'manifest.json')));
 
-  if (isFirefox) {
-    manifest.background.scripts = manifest.background.scripts || [];
-    if (!manifest.background.scripts.includes(manifest.background.service_worker)) {
-      manifest.background.scripts.push(manifest.background.service_worker);
+    if (isFirefox) {
+        manifest.background.scripts = manifest.background.scripts || [];
+        if (!manifest.background.scripts.includes(manifest.background.service_worker)) {
+            manifest.background.scripts.push(manifest.background.service_worker);
+        }
+        delete manifest.background.service_worker;
+
+        manifest.browser_specific_settings = {
+            gecko: {
+                id: manifest.name + '@example.com',
+                data_collection_permissions: {
+                    required: ['none'],
+                },
+            },
+        };
     }
-    delete manifest.background.service_worker;
 
-    manifest.browser_specific_settings = {
-      gecko: { id: manifest.name + '@example.com' },
-    };
-  }
+    await writeFile(join(distPath, 'manifest.json'), JSON.stringify(manifest, null, 2));
 
-  await writeFile(join(distPath, 'manifest.json'), JSON.stringify(manifest, null, 2));
+    await rm(join(root, zipName), { force: true });
 
-  await rm(join(root, zipName), { force: true });
+    // only for macOS
+    if (isChrome) {
+        exec(`zip -r ${zipName} ${distDir}`);
+    }
 
-  // only for macOS
-  if (isChrome) {
-    exec(`zip -r ${zipName} ${distDir}`);
-  }
-
-  if (isFirefox) {
-    exec(`cd ${distPath} && zip -r ../${zipName} *`);
-  }
+    if (isFirefox) {
+        exec(`cd ${distPath} && zip -r ../${zipName} *`);
+    }
 })();
